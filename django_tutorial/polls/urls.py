7f019aa7c3c92935f99a190cee0e482b3f442899
LachlanMarnham <lachlan.marnham@gmail.com>
2022-06-26 18:46:54 +0100

    isort

--- django_tutorial/polls/urls.py
+++ django_tutorial/polls/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
-
 from django_tutorial.polls import views
-
 
 urlpatterns = [
     path('', views.index, name='index'),
--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -76,7 +75,7 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '.data' / 'db.sqlite3',
     }
 }
 
