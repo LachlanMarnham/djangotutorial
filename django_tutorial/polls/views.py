210a97a5eb6a8af057426830f8f0d73802fac4bb
LachlanMarnham <lachlan.marnham@gmail.com>
2022-06-27 14:28:01 +0100

    Update index view and template!

--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django_tutorial.polls.models import Question
+from django.shortcuts import render
 
 
 def index(request):
     latest_questions_query_set = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join(q.question_text for q in latest_questions_query_set)
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_questions_query_set,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
